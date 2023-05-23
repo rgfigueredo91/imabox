a73ffb2e323975dff4d728ccb6d4769a5e58d1d0
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2023-05-23 06:54:23 -0300

    k

--- js/navbarmove.js
+++ js/navbarmove.js
@@ -1,19 +1,21 @@
 let lastScrollTop = 100; 
 		
 			navbar = document.getElementById("navbar-noboots");
+			titleremove = document.getElementById("removeidtitle");
 			window.addEventListener("scroll", function () {
 				var scrollTop = window.pageYOffset || document
 					.documentElement.scrollTop;
 				if (scrollTop > lastScrollTop) {
 					navbar.style.top = "-100px";
 					navbar.style.background = "white";
-		
+					titleremove.style.opacity = "100";
 					
 					
 					
 				} else {
 					navbar.style.top = "0";
 					navbar.style.background = "none";
+					titleremove.style.opacity = "0";
 					
 					
 					
