c9b3f213aee6639475f7663ba6d3b5145c8cda1d
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2023-06-28 13:50:27 -0300

    ,

--- js/translate.js
+++ js/translate.js
@@ -1,15 +1,14 @@
+
 document.addEventListener("DOMContentLoaded", function(e){
     let language = {
         esp: {
-            works: "#trabajos ↓",
-            about: "#nosotros",
-            aboutPhone: "nosotros",
-            contact: "#contacto",
-            contactphone: "contacto",
+            stillImages: "Imágenes",
+            virtual: "Tour Virtual",
+            about: "nosotros",
+            contact: "contacto",
             portfolio: "Vis Arq",
             visualization: "360º",
             rights: `© 2022 Imabox "Unboxing Possible Futures". Todos los derechos reservados`,
-            vr: " Ps R.V",
 
         }
     };
@@ -22,41 +21,23 @@ document.addEventListener("DOMContentLoaded", function(e){
         //MENU PRINCIPAL
         
         //desktop
-        document.getElementById("link-font").textContent = language.esp.works;
+        document.getElementById("link-font").textContent = language.esp.stillImages;
         document.getElementById("link-font-about").textContent = language.esp.about;
-        document.getElementById("contactid").textContent = language.esp.contact;
-        document.getElementById("portfolio-translate").textContent = language.esp.portfolio;
-        document.getElementById("360-translate").textContent = language.esp.visualization;
+        document.getElementById("link-font-Virtual").textContent = language.esp.virtual;
+        document.getElementById("link-font-contact").textContent = language.esp.contact;
+        document.getElementById("link-font-360").textContent = language.esp.visualization;
         document.getElementById("rights-reserved").textContent = language.esp.rights;
-        document.getElementById("vr-translate").textContent = language.esp.vr;
-    
         
-        
-        //phone
-        document.getElementById("contact-translate-phone").textContent = language.esp.contactphone;
-        document.getElementById("portfolio-translate-phone").textContent = language.esp.portfolio;
-        document.getElementById("360-translate-phone").textContent = language.esp.visualization;
-        document.getElementById("psvr-translate-phone").textContent = language.esp.vr;
-        document.getElementById("artist-translate-phone").textContent = language.esp.aboutPhone;
-
         //desktop mantener el #eng cuando hago click en los links 
-
-      
-        document.getElementById("contactid").href = "contact.html#esp";
+        document.getElementById("link-font").href = "works.html#esp";
+        document.getElementById("link-font-contact").href = "contact.html#esp";
         document.getElementById("link-font-about").href = "about.html#esp";
-        document.getElementById("portfolio-translate").href = "index.html#esp";
-        document.getElementById("360-translate").href = "360.html#esp";
-        document.getElementById("vr-translate").href = "vr.html#esp";
+        document.getElementById("link-font-index").href = "index.html#esp";
+        document.getElementById("link-font-360").href = "360.html#esp";
+        document.getElementById("link-font-Virtual").href = "vr.html#esp";
   
 
-          //phone mantener el #eng cuando hago click en los links
-      
-        document.getElementById("artist-translate-phone").href = "about.html#esp";
-        document.getElementById("contact-translate-phone").href = "contact.html#esp";
-        document.getElementById("portfolio-translate-phone").href = "index.html#esp";
-        document.getElementById("360-translate-phone").href = "360.html#esp";
-        document.getElementById("psvr-translate-phone").href = "vr.html#esp";
-      
+
 
         
     }   
