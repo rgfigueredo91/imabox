4f84078e1986ba8f738d8bddc30ffaef997a045c
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2025-11-20 18:09:30 -0300

    qw

---
+++ app-files/data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2025_11_20_360-sector-freeshop",
+      "name": "2025_11_20_360 SECTOR FREESHOP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5593583598609104,
+          "pitch": -0.023090040192997208,
+          "rotation": 0,
+          "target": "4-2025-11-20_1673_recorrido-03"
+        },
+        {
+          "yaw": -1.2811361768834217,
+          "pitch": -0.016336009205687674,
+          "rotation": 0,
+          "target": "2-2025-11-20-vip-aic"
+        },
+        {
+          "yaw": -1.6646110226974287,
+          "pitch": 0.002187149289014201,
+          "rotation": 0,
+          "target": "1-2025-11-19-camara-05"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2025-11-19-camara-05",
+      "name": "2025-11-19 camara 05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1042179077379064,
+          "pitch": 0.004752455285059298,
+          "rotation": 0,
+          "target": "0-2025_11_20_360-sector-freeshop"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2025-11-20-vip-aic",
+      "name": "2025-11-20 VIP AIC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.572575022026381,
+          "pitch": -0.08941371178470092,
+          "rotation": 0,
+          "target": "0-2025_11_20_360-sector-freeshop"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-2025-11-20_360--pv-02",
+      "name": "2025-11-20_360  PV 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.013425609630729696,
+        "pitch": -0.00927633636951164,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07934316766368532,
+          "pitch": -0.013824033418487147,
+          "rotation": 0,
+          "target": "4-2025-11-20_1673_recorrido-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-2025-11-20_1673_recorrido-03",
+      "name": "2025-11-20_1673_Recorrido 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8146341371975225,
+          "pitch": -0.037165883393612376,
+          "rotation": 0,
+          "target": "0-2025_11_20_360-sector-freeshop"
+        },
+        {
+          "yaw": -1.3708508767038303,
+          "pitch": 0.013083176575165112,
+          "rotation": 0,
+          "target": "3-2025-11-20_360--pv-02"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
