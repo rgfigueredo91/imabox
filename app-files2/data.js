8f61a3e9248c3e6f533a7fd475b137e3e0897e57
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2025-12-18 00:22:31 -0300

    ASDA

---
+++ app-files2/data.js
@@ -0,0 +1,175 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-01",
+      "name": "living 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.6012683915458048,
+        "pitch": 0.04861587172507953,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4703440105589012,
+          "pitch": 0.009564367230947113,
+          "rotation": 0,
+          "target": "1-living-02"
+        },
+        {
+          "yaw": -1.853384320292598,
+          "pitch": 0.08143518728448207,
+          "rotation": 0,
+          "target": "2-cuarto-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-02",
+      "name": "living 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.008957318326277,
+          "pitch": 0.09479271935521183,
+          "rotation": 0,
+          "target": "3-bano-01-final"
+        },
+        {
+          "yaw": -1.4980743543418562,
+          "pitch": 0.02453826442396867,
+          "rotation": 0,
+          "target": "0-living-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cuarto-01",
+      "name": "cuarto 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6471323546838939,
+          "pitch": 0.10131359098556914,
+          "rotation": 0,
+          "target": "0-living-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bano-01-final",
+      "name": "bano 01 final",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3496374689164128,
+          "pitch": 0.11713926447267653,
+          "rotation": 0,
+          "target": "1-living-02"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "KIU AV ITALIA",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
